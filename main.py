c9294f4473c4680841928d4c9677a501c35a8201
Gabriel Santos / yKold <108306401+yKold@users.noreply.github.com>
2024-07-11 19:32:35 -0300

    er

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import discord
-
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents = discord.Intents.default()
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
